153141e9acaf59cec8c8b63b06856ac140672615
Yasen <xiaoqizhao@outlook.com>
2020-11-29 06:39:46 -0500

    update

--- 3.0.0/FinCalc.java
+++ 3.0.0/FinCalc.java
@@ -4,6 +4,7 @@ public class FinCalc {
         double assetPrice, downPaymentPercentage, financingAmount, annualRate;
         double installment;
         int terms;
+        String line = "==============================================================================================";
 
         assetPrice = 1200000;
         downPaymentPercentage = 0.2;
@@ -23,12 +24,22 @@ public class FinCalc {
         System.out.println("Monthly Installment Amount: " + installment);
 
         System.out.println("\n======Here is the Payment Plan======");
+        System.out.println(line);
+        System.out.println("Term\tBeginning Balance\tInstallment\tInterest\tPrincipal\tEnding Balance");    
+        System.out.println(line);
 
-        PaymentPlan(financingAmount, installment, annualRate, terms);
+        // Monthly Payment Detail
+        double beginningBalance = financingAmount;
+        for (int i = 1; i <= terms; i++) {
+            double interest = beginningBalance * (annualRate / 12);
+            double principal = installment - interest;
+            double endingBalance = beginningBalance - principal;
+            //System.out.println(i + "\t" + beginningBalance + "\t" + installment 
+            //    + "\t" + interest + "\t" + principal + "\t" + endingBalance);
+            System.out.printf("%d\t%10.2f\t\t%10.2f\t%10.2f\t%10.2f\t%10.2f\n", i, beginningBalance, installment, interest, principal, endingBalance);
+            beginningBalance = endingBalance;
+        }
 
+        System.out.println(line);
     }
-}
-
-public class PaymentPlan(double beginningBalance, double installment, doulbe rate, int terms) {
-
 }
